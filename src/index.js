f2490f95c5fe57b4ec2291bae6054aca9b2e7156
Feruz <feruzprivet@gmail.com>
2020-06-23 23:47:35 +0500

    Created a Card component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card name={robots[0].name} email={robots[0].email} id={robots[0].id} />
   </React.StrictMode>,
   document.getElementById('root')
 );
