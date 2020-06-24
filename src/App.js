3e602dfe506f41842a2f7bf91d581484017daaff
Feruz <feruzprivet@gmail.com>
2020-06-24 19:22:32 +0500

    Created a SearchBox Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import SearchBox from './SearchBox';
+import CardList from './CardList';
+import { robots } from './robots';
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = event => {
+		this.setState({ searchfield: event.target.value});
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter( robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		});
+		return (
+		  	<div className='tc'>
+		  		<h1>Robofriends</h1>
+		  		<SearchBox searchChange={this.onSearchChange} />
+			    <CardList robots={filteredRobots} />
+		  	</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+  	<App />
   </React.StrictMode>,
   document.getElementById('root')
 );
