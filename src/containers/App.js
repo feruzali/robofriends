f670b7f163f9ed3c2cbea694f08f5d830f5a20e2
Feruz <feruzprivet@gmail.com>
2020-06-26 00:17:11 +0500

    Structured the project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import SearchBox from './SearchBox';
-import CardList from './CardList';
-import Scroll from './Scroll';
+import SearchBox from '../components/SearchBox';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
